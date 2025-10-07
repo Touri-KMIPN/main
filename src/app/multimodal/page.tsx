867c1d2115be141b2cc7b9393a16542c888ae359
Kgs. M. Rafiussyakban <rafiussyakban@gmail.com>
2025-10-07 23:50:39 +0700

    feat: Mobile n Dekstop Video Call Layout

--- src/app/multimodal/page.tsx
+++ src/app/multimodal/page.tsx
@@ -2,8 +2,13 @@
 "use client";
 import { useState, useCallback } from "react";
 import CameraPreviewSDK from "./_components/CameraPreviewSDK";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const router = useRouter();
   const [messages, setMessages] = useState<
     { type: "human" | "gemini"; text: string }[]
   >([]);
@@ -14,6 +19,14 @@ export default function Home() {
 
   return (
     <>
+      <div className="fixed top-10 left-5 z-50">
+        <Button
+          className="rounded-full cursor-pointer"
+          onClick={() => router.back()}
+        >
+          <ArrowLeft size={18} />
+        </Button>
+      </div>
       <div className="max-h-dvh flex flex-col items-center justify-center">
         <div className="flex">
           {/* <CameraPreview onTranscription={handleTranscription} /> */}
