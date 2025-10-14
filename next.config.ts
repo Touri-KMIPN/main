093a7b12594c457c88e6a4b6a74881a5a80f709e
Kgs. M. Rafiussyakban <rafiussyakban@gmail.com>
2025-10-14 11:03:40 +0700

    Hardcode Fallback while in offline mode

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 // import withSerwistInit from "@serwist/next";
 
 // const withSerwist = withSerwistInit({
@@ -13,8 +13,33 @@ import type { NextConfig } from "next";
 //   output: "standalone",
 // });
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
+//   output: "standalone",
+// };
+
+// export default nextConfig;
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: "public",
+  fallbacks: {
+    //image: "/static/images/fallback.png",
+    document: "/offline", // if you want to fallback to a custom page rather than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "standalone",
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
